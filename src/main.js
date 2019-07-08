aeb35f89470e56829f485d58dab60988f3fe5d45
Hugh Sherman Donkin and Jacob Irwin <hugh+jacob@>
2019-07-08 11:39:52 -0700

    creates pokemon class and api call methods

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import './styles.css';
 import 'bootstrap';
 import $ from "jquery";
+import "./pokemon-api-caller.js";
---
+++ src/pokemon-api-caller.js
@@ -0,0 +1,108 @@
+export class Pokemon{
+  constructor(name, number, sprite, flavortext, type, height, weight){
+    this.name = name;
+    this.number = number;
+    this.sprite = sprite;
+    this.flavortext = flavortext;
+    this.type = [];
+    this.height = height;
+    this.weight = weight;
+  }
+
+  pokemonNameCall(pokemonName){
+    let pokeNamePromise = new Promise(function(resolve,reject){
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+      let request = new XMLHttpRequest();
+
+      request.onload = function(){
+        if (this.status === 200) {
+          console.log("Api responded!");
+          resolve(request.response);
+        }else {
+          console.log("Rejection!!");
+          reject(Error(request.statusText));
+        }
+      }
+
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    pokeNamePromise.then(function(response){
+      let pokemonCalled = JSON.parse(response);
+      console.log(pokemonCalled);
+      this.name = pokemonCalled.name;
+      this.number = pokemonCalled.id;
+      this.sprite = pokemonCalled.sprites.front_default;
+      this.flavortext = "";
+      // pushing each type to the this.type array;
+      pokemonCalled.types.forEach(function(arrayItem){
+        this.type.push(arrayItem.type.name);
+      })
+      this.height = pokemonCalled.height;
+      this.weight = pokemonCalled.weight;
+
+    })
+
+  }
+
+
+
+  pokemonSpeciesCall(pokemonName){
+    let pokeSpeciesPromise = new Promise(function(resolve,reject){
+      const url = `https://pokeapi.co/api/v2/pokemon-species/${pokemonName}`;
+      let request = new XMLHttpRequest();
+
+      request.onload = function(){
+        if (this.status === 200) {
+          console.log("Api responded!");
+          resolve(request.response);
+        }else {
+          console.log("Rejection!!");
+          reject(Error(request.statusText));
+        }
+      }
+
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    pokeSpeciesPromise.then(function(response){
+      let pokemonCalled = JSON.parse(response);
+      console.log(pokemonCalled);
+      return pokemonCalled;
+    })
+
+  }
+
+
+
+  pokemonTypeCall(pokemonType){
+    let pokeTypePromise = new Promise(function(resolve,reject){
+      const url = `https://pokeapi.co/api/v2/type/${pokemonType}`;
+      let request = new XMLHttpRequest();
+
+      request.onload = function(){
+        if (this.status === 200) {
+          console.log("Api responded!");
+          resolve(request.response);
+        }else {
+          console.log("Rejection!!");
+          reject(Error(request.statusText));
+        }
+      }
+
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    pokeTypePromise.then(function(response){
+      let typelistCalled = JSON.parse(response);
+      console.log(typelistCalled);
+      return typelistCalled;
+    })
+
+  }
+
+
+}
