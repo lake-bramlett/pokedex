64950684888832d034fb286ead1c0a5654aac860
Student <example@example.com>
2019-07-09 09:47:12 -0700

    updates weight-height-calc

---
+++ src/weight-height-calc.js
@@ -0,0 +1,9 @@
+export function convertWeight(inputWeight){
+  let newWeight = inputWeight*(0.1);
+  return newWeight;
+}
+
+export function convertHeight (inputHeight){
+  let newHeight = inputHeight*(0.1);
+  return newHeight;
+}
