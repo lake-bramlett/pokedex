d1fdd5058eb2b46da08e1994becdc4bb06f440bd
jirwincline <jakobinwins@gmail.com>
2019-07-09 15:55:51 -0700

    add pokemon flip animation

--- src/main.js
+++ src/main.js
@@ -9,17 +9,17 @@ import { Pokemon } from "./pokemon-api-caller.js";
 import { displayImg } from './display.js';
 import './randompokemon.js'
 import "./weight-height-calc.js";
-import { bootUp,blinkingButtons } from './animations.js';
+import { bootUp,blinkingButtons,imgAnimation } from './animations.js';
 
 
 
 
 $(document).ready(function() {
+  let pokemonSearch = new Pokemon();
   setTimeout(function(){
     bootUp();
   }, 3000);
   $('.d-center').click(function(){
-    let pokemonSearch = new Pokemon();
     let name = $('#name').val();
     pokemonSearch.pokemonNameCall(name);
     console.log(pokemonSearch)
@@ -28,5 +28,9 @@ $(document).ready(function() {
       console.log('trying to display')
     });
 
+  });
+  $('.sprite-container').on('click', function(){
+    console.log('clicked');
+    pokemonSearch.imgAnimation();
   });
 });
