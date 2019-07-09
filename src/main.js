addeb0bc7e132de14793fa88f7b57282450ed755
Student <example@example.com>
2019-07-09 10:15:28 -0700

    We did it for random selection of pokemon today

--- src/main.js
+++ src/main.js
@@ -5,3 +5,4 @@ import "./pokemon-api-caller.js";
 import "./flavortext.js";
 import { Pokemon } from "./pokemon-api-caller.js";
 import { displayImg } from './display.js';
+import './randompokemon.js'
