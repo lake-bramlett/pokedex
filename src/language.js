c88ac52ce0a2fb76f10e3c478e6004f3a828728c
jIrwinCline <example@example.com>
2019-07-10 15:24:34 -0700

    add simple line of code to change userLang variable and therfore change language

---
+++ src/language.js
@@ -0,0 +1,17 @@
+import $ from "jquery";
+import "./pokemon-api-caller.js";
+import "./flavortext.js";
+import { Pokemon, PokemonListByType } from "./pokemon-api-caller.js";
+
+// Pokemon.prototype.userLangChange = function(languageVal) {
+//   userLang = languageVal
+// }
+
+
+
+
+//------------testing
+//has to be inside submit where 'pokemonSearch' is defined
+//--------------------------------------------------------
+// userLang = $('#language').val();
+// pokemonSearch.userLangChange(inputLanguage);
